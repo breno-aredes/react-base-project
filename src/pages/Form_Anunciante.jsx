6fb454eee115a0acb8bb227ad89f3496dfd6ff82
artic <migsveiga@gmail.com>
2024-06-13 23:06:30 -0300

    Mudança Basica no Forms Anunciante

--- src/pages/Form_Anunciante.jsx
+++ src/pages/Form_Anunciante.jsx
@@ -19,46 +19,50 @@ function Form_Anunciante() {
         <Banner titulo="Formulário" imagem={"imagens/heroHome.png"}/>
         <Style>
             <main className="Conteudo">
-                <section className="Form-Contato">
-                    <div className="Form-Contato-Escrito">
-                            <h3 className="Form-Contato-Escrito-Titulo">Contato</h3>
 
-                            {ElementosContato.map(
-                            (e, index) => (
-                                <Forms className=" FormsContato"
-                                    key={index}
-                                    name={e.name}
-                                    type={e.type}
-                                    text={e.text}
-                                    placeholder={e.placeholder}
-                        />
-                        )
-                        )  
-                        } 
-                    </div>
-
+                <section className="FraseEfeito">
+                    <h2> Se interessou? Que tal se juntar a nós e começar a anunciar hoje mesmo?</h2>
                 </section>
 
-                    
-
+                <section className="Form-Contato">
+                    <div className="Form-Contato-Escrito">
+                    <h2>Contato:</h2>
+                        <form>
+                            <label htmlFor="nome">Nome:</label><br />
+                            <input type="text" id="nome" name="nome" placeholder="Insira seu Nome"/><br />
+                            
+                            <label htmlFor="email">Email:</label><br />
+                            <input type="email" id="email" name="email" placeholder="Insira o seu Email" /><br />
+                            
+                            <label htmlFor="telefone">Telefone:</label><br />
+                            <input type="tel" id="telefone" name="telefone" placeholder="Insira o seu Telefone" /><br />
+                        </form>
+                    </div>
+                </section>
 
                 <section className="Form-Endereco">
                         <div className="Form-Endereco-Escrito">
+                        <h2>Endereço:</h2>
+                        
+                            <form>
+                                <label htmlFor="rua">Rua:</label><br />
+                                <input type="text" id="rua" name="rua" placeholder="Insira sua Rua" /><br />
 
-                            <h3 className="Form-Endereco-Escrito-Titulo">Endereço</h3>
+                                <label htmlFor="bairro">Bairro:</label><br />
+                                <input type="text" id="bairro" name="bairro" placeholder="Insira seu Bairro" /><br />
 
-                            {ElementosEndereco.map(
-                                (e, index) => (
-                                    <Forms className=" FormsEndereco"
-                                        key={index}
-                                        name={e.name}
-                                        type={e.type}
-                                        text={e.text}
-                                        placeholder={e.placeholder}
-                            />
-                            )
-                            )  
-                            } 
+                                <label htmlFor="complemento">Complemento:</label><br />
+                                <input type="text" id="complemento" name="complemento" placeholder="Insira o Complemento" /><br />
+
+                                <label htmlFor="cep">CEP:</label><br />
+                                <input type="text" id="cep" name="cep" placeholder="Insira seu CEP" /><br />
+
+                                <label htmlFor="cidade">Cidade:</label><br />
+                                <input type="text" id="cidade" name="cidade" placeholder="Insira sua Cidade" /><br />
+
+                                <label htmlFor="sobre">Sobre Você:</label><br />
+                                <textarea id="sobre" name="sobre" rows="4" placeholder="Conte um pouco sobre você"></textarea><br />
+                            </form>
 
                         </div>
                     </section>
