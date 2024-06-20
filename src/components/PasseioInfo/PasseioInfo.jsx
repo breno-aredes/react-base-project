0dd0adcb05deafbdedfab5d4502e5640adc31e1f
paula-luiza <paulalro8@gmail.com>
2024-06-19 21:17:10 -0300

    formatando página passeios destaque

--- src/components/PasseioInfo/PasseioInfo.jsx
+++ src/components/PasseioInfo/PasseioInfo.jsx
@@ -1,12 +1,36 @@
-const PasseioInfo = ({nome, passeioId, local, dia, mes, hora, key, descricao, guia, valor, imagem, passeio}) => {
+import { PasseioInfoStyle } from "./Style";
 
+
+const PasseioInfo = ({nome, passeioId, local, dia, mes, hora, key, descricao, guia, valor, imagem, passeio}) => {  
   
-  
+
+  const handleVoltarClick = (e) => {
+      window.location.href = `/home#/#passeio`
+  };
+
     return (
-    <div>
-      Passeio Info: {passeioId}
-      Nome: {nome}
+    <PasseioInfoStyle>
+      <div className="card_descricao_passeio"> 
+        <div className="img_container">
+          <img src={imagem} alt={nome} />
+        </div>
+        
+        <div className="infos_passeio_container">
+        <h2>{nome}</h2>
+        <p>
+          <span className="neg">Local: </span>{local} <br />
+          <span className="neg">Dia:</span> {dia} / {mes} <br />
+          <span className="neg">Hora:</span> {hora} <br />
+          <span className="neg">Guia:</span> {guia}
+        </p>
+        
+        <p>{descricao}</p>
+        
+        <p><span className="neg">Preço:</span> {valor}</p>
+        <button className="btn_voltar" onClick={handleVoltarClick}>Voltar</button>
+        </div>
       </div>
+    </PasseioInfoStyle>
     )
   
   };
