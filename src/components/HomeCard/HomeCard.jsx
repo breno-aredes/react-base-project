4cf851a19ffaca4bb046e8fbdbf6cb43b7c9870b
breno.aredes <breno_mg10@hotmail.com>
2024-08-01 22:05:50 -0300

    fix: for deploy

--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -1,42 +1,57 @@
 import { HomePageHead } from "./Style";
 import Botao from "../Botao/Botao";
 
-import '../../utils/i18n'
+import "../../utils/i18n";
 import { useTranslation } from "react-i18next";
 
 import { FaApple } from "react-icons/fa";
 import { FaGooglePlay } from "react-icons/fa";
 
-const HomeCard = ({titulo, titulo1, texto, pl}) => {
+const HomeCard = ({ titulo, titulo1, texto, pl }) => {
   const { t } = useTranslation();
 
-    return(
-      <HomePageHead>
-        <div className="background" />
-        <div className="overlay" />
-        <section className="container">
-          <div className="text-container">
-            <div className="title-container">
-              <h2 className="titulo">{titulo1}</h2>
-              <h1 className="titulo">{titulo}</h1>
-            </div>
-            <h3 className="subtitulo">{texto}</h3>
-            <div className="textoplataformas"> <h5>{pl}</h5> </div>
-            <span className="botoesPlataformas">
-              <a href="https://www.apple.com/br/app-store/" target="_blank" rel="noopener noreferrer">
-                <button className="botao"><FaApple size={25}/> App Store</button>
-              </a>
-              <a href="https://play.google.com/store/apps" target="_blank" rel="noopener noreferrer">
-                <button className="botao"><FaGooglePlay size={25}/> Google Play</button>
-              </a>
-            </span>
+  return (
+    <HomePageHead>
+      <div className="background" />
+      <div className="overlay" />
+      <section className="container">
+        <div className="text-container">
+          <div className="title-container">
+            <h2 className="titulo">{titulo1}</h2>
+            <h1 className="titulo">{titulo}</h1>
           </div>
-          <div className="img-container">
-            <img src="imagens/PasseioCariocaLogoBranco.svg" alt="Logo"/> 
+          <h3 className="subtitulo">{texto}</h3>
+          <div className="textoplataformas">
+            {" "}
+            <h5>{pl}</h5>{" "}
           </div>
-        </section>
+          <span className="botoesPlataformas">
+            <a
+              href="https://www.apple.com/br/app-store/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="botao">
+                <FaApple size={25} /> App Store
+              </button>
+            </a>
+            <a
+              href="https://play.google.com/store/apps"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="botao">
+                <FaGooglePlay size={25} /> Google Play
+              </button>
+            </a>
+          </span>
+        </div>
+        <div className="img-container">
+          <img src="imagens/PasseioCariocaLogoBranco.svg" alt="Logo" />
+        </div>
+      </section>
     </HomePageHead>
-    )
-  }
-  
-  export default HomeCard;
+  );
+};
+
+export default HomeCard;
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const webpack = require("webpack");
 const path = require("path");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const package = require('./package.json')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const package = require("./package.json");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "./build",)
+    path: path.resolve(__dirname, "./build"),
   },
 
   module: {
@@ -16,40 +16,36 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      }
-    ]
+      },
+    ],
   },
   resolve: {
     extensions: [".js", ".jsx"],
   },
   plugins: [
-    new CopyWebpackPlugin(
-      {
-        patterns: [
-          {
-            from: 'public',
-            to: './'
-          }
-        ]
-      }
-    ),
-    new webpack.DefinePlugin(
-      {
-        "process.env": JSON.stringify(process.env),
-        "process.env.PUBLIC_URL": JSON.stringify(package.homepage)
-      }
-    )
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "./",
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+      "process.env.PUBLIC_URL": JSON.stringify(package.homepage),
+    }),
   ],
   devServer: {
     open: true,
     historyApiFallback: true,
     static: {
-      directory: path.resolve(__dirname, './public')
-    }
-  }
+      directory: path.resolve(__dirname, "./public"),
+    },
+  },
 };
